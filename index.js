b0e4418edd4c31020567e191fc403395d9a8e1c2
Luis Alfonso Alcántara López Ortega <30807768+luisalcantaralo@users.noreply.github.com>
2019-04-28 09:30:26 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 app.use(cors());
+
 if(!config.get('jwtPrivateKey')){
   console.error('FATAL ERROR: jwtPrivateKey is not defined.');
   process.exit(1);
